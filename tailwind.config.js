e0985250620c799fa000a9bb25356700c79043c7
vienpham <viendev@outlook.com>
2024-05-10 15:52:43 +0700

    feat: update eslint, tailwincss

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,18 +15,18 @@ export default {
     extend: {
       colors: {
         background: 'hsl(var(--background))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
+        // primary: {
+        //   DEFAULT: 'hsl(var(--primary))',
+        //   foreground: 'hsl(var(--primary-foreground))',
+        // },
+        // secondary: {
+        //   DEFAULT: 'hsl(var(--secondary))',
+        //   foreground: 'hsl(var(--secondary-foreground))',
+        // },
+        // accent: {
+        //   DEFAULT: 'hsl(var(--accent))',
+        //   foreground: 'hsl(var(--accent-foreground))',
+        // },
       },
       borderRadius: {
         lg: 'var(--radius)',
